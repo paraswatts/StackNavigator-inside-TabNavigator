c9ebb84b9d5c68b24d8eedb42fe23e13ea2ff7a8
paraswatts <paras.watts@gmail.com>
2017-09-11 12:58:56 +0530

    Add files via upload

---
+++ Tabs/Tab1.js
@@ -0,0 +1,109 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity
+} from 'react-native';
+var {height, width} = Dimensions.get('window');
+import { StackNavigator } from 'react-navigation';
+
+export default class Tab1 extends Component {
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={{
+            width: width-20,
+          alignItems:'center',
+          margin:10,
+          borderWidth:1,
+          height: 50
+        }}
+        onPress={ () => navigate('Tab1Page1')}
+        >
+          <Text>
+            Page1
+          </Text>
+
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            width: width-20,
+          alignItems:'center',
+          margin:10,
+          borderWidth:1,
+          height: 50
+        }}
+        onPress={ () => navigate('Tab1Page2')}
+         >
+          <Text>
+            Page2
+          </Text>
+
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            width: width-20,
+          alignItems:'center',
+          margin:10,
+          borderWidth:1,
+          height: 50
+        }} 
+        onPress={ () => navigate('Tab1Page3')}
+        >
+          <Text>
+            Page3
+          </Text>
+
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+          width: width-20,
+          alignItems:'center',
+          margin:10,
+          borderWidth:1,
+          height: 50
+        }}
+        onPress={ () => navigate('Tab1Page4')}
+        >
+          <Text>
+            Page4
+          </Text>
+
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+});
+
---
+++ Tabs/Tab4.js
@@ -0,0 +1,107 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity
+} from 'react-native';
+var {height, width} = Dimensions.get('window');
+export default class Tab4 extends Component {
+  render() {
+    const { navigate } = this.props.navigation;
+    
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={{
+            width: width-20,
+          alignItems:'center',
+          margin:10,
+          borderWidth:1,
+          height: 50
+        }}
+        onPress={ () => navigate('Tab4Page1')}
+        >
+          <Text>
+            Page1
+          </Text>
+
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            width: width-20,
+          alignItems:'center',
+          margin:10,
+          borderWidth:1,
+          height: 50
+        }} 
+        onPress={ () => navigate('Tab4Page2')}
+        >
+          <Text>
+            Page2
+          </Text>
+
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            width: width-20,
+          alignItems:'center',
+          margin:10,
+          borderWidth:1,
+          height: 50
+        }}
+        onPress={ () => navigate('Tab4Page3')}
+        >
+          <Text>
+            Page3
+          </Text>
+
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+          width: width-20,
+          alignItems:'center',
+          margin:10,
+          borderWidth:1,
+          height: 50
+        }}
+        onPress={ () => navigate('Tab4Page4')}
+        >
+          <Text>
+            Page4
+          </Text>
+
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+});
